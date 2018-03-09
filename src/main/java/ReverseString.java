27476d3216a0a83ee01b80cf6d44bad52499d36c
axel <const.afanasyev@gmail.com>
2018-03-09 20:00:20 +0200

    1.2.5 change ReverseString a little

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -2,14 +2,13 @@ public class ReverseString {
     public static String reverseString(String string){
         char[] reversed = string.toCharArray();
 
-        int length = string.length();
+        char temp;
+        int i, j;
 
-        int temp, i, j;
-
-        for (i = 0, j = length-1; i < j; i++, j--){
+        for (i = 0, j = string.length()-1; i < j; i++, j--){
             temp = reversed[i];
             reversed[i] = reversed[j];
-            reversed[j] = (char)temp;
+            reversed[j] = temp;
         }
 
         return String.valueOf(reversed);
